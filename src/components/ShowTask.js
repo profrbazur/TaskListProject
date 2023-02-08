8c6c982a4030a2a1bd1c28aa3e648cab2d5c4bce
profrbazurph <profrbazurph@gmail.com>
2023-02-09 05:15:42 +0800

    Edit and Delete

--- src/components/ShowTask.js
+++ src/components/ShowTask.js
@@ -1,27 +1,36 @@
-export const ShowTask = ({ tasklist, setTasklist }) => {
+export const ShowTask = ({tasklist, setTasklist, task, setTask}) => {
+
+    const handleEdit = (id) => {
+        const seletedTask = tasklist.find(todo => todo.id === id);
+        setTask(seletedTask);
+    }
+
+    const handleDelete = (id) => {
+        const updatedTasklist = tasklist.filter(todo => todo.id !== id);
+        setTasklist(updatedTasklist);
+    }
+
   return (
     <section className="showTask">
-      <div className="head">
-        <div>
-          <span className="title">Todo</span>
-          <span className="count">{tasklist.length}</span>
+        <div className="head">
+            <div>
+                <span className="title">Todo</span>
+                <span className="count">{tasklist.length}</span>
+            </div>
+            <button onClick={() => setTasklist([])} className="clearAll">Clear All</button>
         </div>
-        <button className="clearAll" onClick={() => setTasklist([])}>
-          Clear All
-        </button>
-      </div>
-      <ul>
-        {tasklist.map((task) => (
-          <li key={task.id}>
-            <p>
-              <span className="name">{task.name}</span>
-              <span className="time">{task.time}</span>
-            </p>
-            <i className="bi bi-pencil-square"></i>
-            <i className="bi bi-trash"></i>
-          </li>
-        ))}
-      </ul>
+        <ul>
+            { tasklist.map((todo) => (
+                <li key={todo.id}>
+                    <p>
+                        <span className="name">{todo.name}</span>
+                        <span className="time">{todo.time}</span>
+                    </p>
+                    <i onClick={() => handleEdit(todo.id)} className="bi bi-pencil-square"></i>
+                    <i onClick={() => handleDelete(todo.id)} className="bi bi-trash"></i>
+                </li>
+            )) }            
+        </ul>
     </section>
-  );
-};
+  )
+}
