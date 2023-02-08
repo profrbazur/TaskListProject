8adc0203548e25aaf6bc79dea2bbb013f8e6fb76
profrbazurph <profrbazurph@gmail.com>
2023-02-09 06:21:05 +0800

    Local Storage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import { useState } from "react";
-import { Header } from "./components/Header";
-import { AddTask } from "./components/AddTask";
-import { ShowTask } from "./components/ShowTask";
+import { useEffect, useState } from 'react';
+import { Header } from './components/Header';
+import { AddTask } from './components/AddTask';
+import { ShowTask } from './components/ShowTask';
 import './App.css';
 
-
 function App() {
-  const [tasklist, setTasklist] = useState([]);
+  const [tasklist, setTasklist] = useState(
+    JSON.parse(localStorage.getItem('tasklist')) || []
+  );
   const [task, setTask] = useState({});
+
+  useEffect(() => {
+    localStorage.setItem('tasklist', JSON.stringify(tasklist));
+  }, [tasklist]);
 
   return (
     <div className="App">
       <Header />
-      <AddTask 
-        tasklist={tasklist} 
-        setTasklist={setTasklist} 
+      <AddTask
+        tasklist={tasklist}
+        setTasklist={setTasklist}
         task={task}
         setTask={setTask}
       />
-      <ShowTask 
-        tasklist={tasklist} 
+      <ShowTask
+        tasklist={tasklist}
         setTasklist={setTasklist}
         task={task}
-        setTask={setTask}  
+        setTask={setTask}
       />
     </div>
   );
