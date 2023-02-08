f94731f3a4392bea44a7b84b2b58e880d0cd537c
profrbazurph <profrbazurph@gmail.com>
2023-02-09 05:34:36 +0800

    Edit and Delete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,20 @@
-import Logo from "../assets/logo.png"
+import Logo from '../assets/logo.png';
 
 export const Header = () => {
   return (
     <header>
-        <div className="logo">
-            <img src={Logo} alt="Taskmate Logo" />
-            <span>Taskmate</span>
-        </div>
-        <div className="themeSelector">
-            <span className="light"></span>
-            <span className="medium"></span>
-            <span className="dark activeTheme"></span>
-            <span className="gOne"></span>
-            <span className="gTwo"></span>
-            <span className="gThree"></span>
-        </div>
+      <div className="logo">
+        <img src={Logo} alt="Kodego Logo" />
+        <span>TaskList</span>
+      </div>
+      <div className="themeSelector">
+        <span className="light"></span>
+        <span className="medium"></span>
+        <span className="dark activeTheme"></span>
+        <span className="gOne"></span>
+        <span className="gTwo"></span>
+        <span className="gThree"></span>
+      </div>
     </header>
-  )
-}
+  );
+};
